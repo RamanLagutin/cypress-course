e352ba036e08c96d0c77764a35443ae21ec117b5
Raman Lahutsin <roman.lagutin.work@yandex.com>
2024-01-23 12:23:56 +0400

    Application

---
+++ cypress/tests/ui/auth.spec.ts
@@ -0,0 +1,23 @@
+describe("User Sign-up and Login", function () {
+  it("should redirect unauthenticated user to signin page", function () {
+  });
+
+  it("should redirect to the home page after login", function () {
+  });
+
+  it("should remember a user for 30 days after login", function () {
+    // check cookie `connect.sid` and its property `expiry` 
+  });
+
+  it("should allow a visitor to sign-up, login, and logout", function () {
+  });
+
+  it("should display login errors", function () {
+  });
+
+  it("should error for an invalid user", function () {
+  });
+
+  it("should error for an invalid password for existing user", function () {
+  });
+});
---
+++ cypress/tests/ui/bankaccounts.spec.ts
@@ -0,0 +1,21 @@
+describe("Bank Accounts", function () {
+  it("creates a new bank account", function () {
+  });
+
+  it("should display bank account form errors", function () {
+    /** Routing number input validations **/
+    // Required field
+    // Min 9 digit
+
+    /** Account number input validations **/
+    // Required field
+    // Min 9 digit
+    // Max 12 gdigit
+  });
+
+  it("soft deletes a bank account", function () {
+  });
+
+  it("renders an empty bank account list state with onboarding modal", function () {
+  });
+});
