e6669acb6d0167ef974a60c0208879f01d9f2349
Raman Lahutsin <roman.lagutin.work@yandex.com>
2024-02-15 21:10:58 +0400

    Current timestamp function

--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -1,3 +1,10 @@
 export const isMobile = () => {
   return Cypress.config("viewportWidth") < Cypress.env("mobileViewportWidthBreakpoint");
 };
+
+export function currentTimestampInSeconds() {
+  const date = new Date();
+  const plus30Days = date.setDate(date.getDate() + 30);
+  const currentTimestamp = Math.floor(plus30Days / 1000);
+  return Math.floor(currentTimestamp / 10);
+}
