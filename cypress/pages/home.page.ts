a65161df99ca0dd7f0985622e6b6c51e24b5a285
Raman Lahutsin <roman.lagutin.work@yandex.com>
2024-03-04 10:41:54 +0400

    Home Page

--- cypress/pages/home.page.ts
+++ cypress/pages/home.page.ts
@@ -1,9 +1,13 @@
 const homeLocator = {
   signOutButton: "[data-test='sidenav-signout']",
+  bankAccountsButton: "[data-test='sidenav-bankaccounts']",
 };
 
 export const homePage = {
   signout: () => {
     cy.get(homeLocator.signOutButton).click();
+  },
+  openBankAccounts: () => {
+    cy.get(homeLocator.bankAccountsButton).click();
   },
 };
