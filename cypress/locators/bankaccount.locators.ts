5ed0a8452b0873ef6316a2a34f14057dd2d1211d
Raman Lahutsin <roman.lagutin.work@yandex.com>
2024-03-04 10:41:43 +0400

    Bank Account page and Locators

---
+++ cypress/locators/bankaccount.locators.ts
@@ -0,0 +1,16 @@
+export const bankAccountLocator = {
+  bankName: "#bankaccount-bankName-input",
+  routingNumber: "#bankaccount-routingNumber-input",
+  accountNumber: "#bankaccount-accountNumber-input",
+  submitButton: "[data-test='bankaccount-submit']",
+  createButton: "[data-test='bankaccount-new']",
+  bankAccountsList: "[data-test='bankaccount-list']",
+  deleteButton: "[data-test='bankaccount-delete']",
+  createBankAccountForm: "[data-test='bankaccount-form']",
+  bankNameError: "#bankaccount-bankName-input-helper-text",
+  routingNumberError: "#bankaccount-routingNumber-input-helper-text",
+  accountNumberError: "#bankaccount-accountNumber-input-helper-text",
+  bankAccount: "[data-test='bankaccount-list-item']",
+};
+
+export const createBankAccountInputs = ["bankName", "routingNumber", "accountNumber"];
